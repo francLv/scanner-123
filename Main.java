85a2b1c9409c905b3731a0c99f9ea1512ee05f8d
Vladimir Levin <94986766+francLv@users.noreply.github.com>
2022-01-23 16:58:28 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner L = new Scanner (System.in);
+    System.out.print ("Input anything somebody: ");
+    String i = L.nextLine();
+    System.out.println (i);
+    }
+  }
